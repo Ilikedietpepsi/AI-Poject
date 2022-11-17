01e964ab61aac1b2c0e0bb5471f25acdbe1b550f
Ilikedietpepsi <jarvishe210@gmail.com>
2022-11-17 16:23:12 -0500

    implement the agent that can beat random agent

--- agents/student_agent.py
+++ agents/student_agent.py
@@ -1,4 +1,8 @@
 # Student agent: Add your own agent here
+from copy import deepcopy
+
+import numpy as np
+
 from agents.agent import Agent
 from store import register_agent
 import sys
@@ -13,6 +17,7 @@ class StudentAgent(Agent):
 
     def __init__(self):
         super(StudentAgent, self).__init__()
+        self.max = 0
         self.name = "StudentAgent"
         self.dir_map = {
             "u": 0,
@@ -20,6 +25,16 @@ class StudentAgent(Agent):
             "d": 2,
             "l": 3,
         }
+
+    def find_walls(self, chess_board, pos):
+        walls = 0
+        pos_r, pos_c = pos
+        for i in range(4):
+            if chess_board[pos_r, pos_c, i]:
+                walls += 1
+        return walls
+
+
 
     def step(self, chess_board, my_pos, adv_pos, max_step):
         """
@@ -36,5 +51,48 @@ class StudentAgent(Agent):
 
         Please check the sample implementation in agents/random_agent.py or agents/human_agent.py for more details.
         """
-        # dummy return
-        return my_pos, self.dir_map["u"]
+        self.max = max_step
+        ori_pos = deepcopy(my_pos)
+        moves = ((-1, 0), (0, 1), (1, 0), (0, -1))
+        my_r, my_c = my_pos
+        adv_r, adv_c = adv_pos
+        my_walls = self.find_walls(chess_board, my_pos)
+        adv_walls = self.find_walls(chess_board, adv_pos)
+
+        state_queue = [(my_pos, 0)]
+        visited = {tuple(my_pos)}
+
+        while state_queue:
+            cur = state_queue.pop(0)
+            cur_pos, cur_step = cur
+            print(cur)
+            r,c = tuple(cur_pos)
+            if cur_step == max_step:
+                break
+            if self.find_walls(chess_board, cur_pos) == 0:
+                my_pos = (r,c)
+                break
+            for dir, move in enumerate(moves):
+                if chess_board[r, c, dir]:
+                    continue
+                m_r, m_c = move
+                next_pos = (r+m_r, c+m_c)
+                if next_pos == adv_pos or tuple(next_pos) in visited:
+                    continue
+
+                visited.add(tuple(next_pos))
+                state_queue.append((next_pos, cur_step + 1))
+
+        dir = np.random.randint(0, 4)
+        r, c = my_pos
+        while chess_board[r, c, dir]:
+            dir = np.random.randint(0, 4)
+        return my_pos, dir
+
+        # for i in range(4):
+        #     if not chess_board[adv_r, adv_c, i]:
+        #         m_r, m_c = moves[i]
+        #         if adv_r + m_r - my_r + adv_c + m_c - my_c <= max_step:
+        #             my_pos = (adv_r + m_r, adv_c + m_c)
+        #             dir = (i+2) % 4
+        #             break
---
+++ cell.py
@@ -0,0 +1,6 @@
+class Cell:
+    def __init__(self, r, c, g, h):
+        self.row = r
+        self.col = c
+        self.g = g
+        self.h = h
